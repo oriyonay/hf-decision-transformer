a5c2714d60855deb792386bca452b5ebe9c8624c
Ori Yonay <oriyonay12@gmail.com>
2023-11-18 17:50:44 -0600

    Update decision_transformer.py

--- gym/decision_transformer/models/decision_transformer.py
+++ gym/decision_transformer/models/decision_transformer.py
@@ -9,6 +9,7 @@ from decision_transformer.models.model import TrajectoryModel
 from decision_transformer.models.trajectory_gpt2 import GPT2Model
 from decision_transformer.models.test_pref import get_preferences
 
+import JaxPref
 
 class DecisionTransformer(TrajectoryModel):
 
